a8788d6fabe02fc455bf22e4777d07e6142f735c
Pham Doan Tony Quyen <tonyquyen2020bt@gmail.com>
2026-09-25 14:15:12 +0700

    update deployment pipeline and configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3TV - Nơi hành trình về nhà bắt đầu | Bất động sản cao cấp và uy tín</title>
+    <title>3TV - Nơi hành trình về nhà  | Bất động sản cao cấp & uy tín</title>
     
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
